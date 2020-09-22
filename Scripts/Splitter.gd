bce0dc576e1d1b760c7b4d3e4015d9f138570223
oded_ro987 <oded_ro987>
2020-09-22 13:55:00 +0200

    updated Splitter's update_value function

--- Scripts/Splitter.gd
+++ Scripts/Splitter.gd
@@ -5,3 +5,14 @@ func _ready():
 	type = Enums.LOGIC_TYPE.SPLITTER
 	create_slots(1)
 	adjust_slot_locations()
+
+#Updates value and sprite:
+func update_value():
+	value = in_slots[0].value
+	out_slot.value = value
+	if value:
+		sprite.frame = 1
+	else:
+		sprite.frame = 0
+	if out_slot.connected_node != null:
+		out_slot.connected_node.emit_signal("on_value_changed", value)
